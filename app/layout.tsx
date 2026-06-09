c0aa3a62aadb2608d695e4429f0fb9e020e7f27f
Kapilla Dev <kingkai12@gmail.com>
2026-06-09 23:25:40 +0300

    Add favicon.ico and apple-touch-icon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,12 @@ export const metadata:Metadata = {
   title: "AFAR MKOPO FASTA",
   description: "Fast, reliable microfinance",
   icons: {
-    icon: '/logo.png',
-    apple: '/logo.png'
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any', type: 'image/x-icon' }
+    ],
+    apple: '/apple-touch-icon.png'
   }
 };
 
